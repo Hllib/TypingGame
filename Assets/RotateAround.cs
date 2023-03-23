f8a325dd5e5ee96bd874421de4c0e5ee8c45c57e
Hlib <hlib.monastyrov@gmail.com>
2023-03-23 18:03:17 +0200

    added Menu scene and key rotation

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject pivotPoint;
+
+    public int rotationAxisZ;
+    public int rotationAxisX;
+    public int rotationAxisY;
+
+    private void Update()
+    {
+        transform.RotateAround(pivotObject.transform.position, new Vector3(rotationAxisX, rotationAxisY, rotationAxisZ), rotationSpeed * Time.deltaTime);   
+    }
+}
