f18d8d5c2d72db34740e858b942f720be611c677
Hlib <hlib.monastyrov@gmail.com>
2023-04-04 20:49:57 +0300

    gameover conditions altered

--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -8,4 +8,22 @@ public class Countdown : MonoBehaviour
     {
         GameManager.Instance.ReloadGame();
     }
+
+    private int _triggerCounter = 1;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (_triggerCounter % 2 == 0)
+            {
+                UIManager.Instance.StopCountDown();
+            }
+            else
+            {
+                UIManager.Instance.StartCountDown();
+            }
+            _triggerCounter += 1;
+        }
+    }
 }
